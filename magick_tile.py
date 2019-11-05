b1a1b5666da3319177ff32c1d4301839e326aec8
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-11-05 14:21:14 -0500

    some more comments

--- magick_tile.py
+++ magick_tile.py
@@ -28,6 +28,7 @@ class Tiler:
         self.id = id
         self.tile_size = tile_size
 
+        # Measure the original dimensions of the image, and find the smaller of the two
         self.orig_dims = [
             int(d)
             for d in subprocess.run(
@@ -71,6 +72,11 @@ class Tiler:
         return [s for s in self.BASE_SMALLER_SIZES if s < self.orig_dims[0]]
 
     def generate_cropped_tiles(self, output_dir):
+        """
+        Two-stage generation:
+
+        1. Use convert's -crop function to write tilesets at each of the scaling factors appropriate for the image (e.g. tiles at 256, 512, 1024 px on a side, etc.) Stores intermediate files to a temporary directory
+        """
         temp_croppath = TemporaryDirectory()
         for sf in tqdm(self.get_scaling_factors(), desc="Creating scaling levels"):
             cropsize = self.tile_size * sf
@@ -82,13 +88,16 @@ class Tiler:
                 f"{cropsize}x{cropsize}",
                 "-set",
                 "filename:tile",
-                "%[fx:page.x],%[fx:page.y],%[fx:w],%[fx:h]",
+                "%[fx:page.x],%[fx:page.y],%[fx:w],%[fx:h]",  # rely on Imagemagick to tell us the resulting dimensions for the tiles it makes, which is especially useful on the non-square tiles from the right and bottom edges of images
                 "+repage",
                 "+adjoin",
                 f"{temp_croppath.name}/{cropsize},{sf},%[filename:tile].jpg",
             ]
             crop_res = subprocess.call(call, stdout=subprocess.PIPE)
 
+        """
+        2. Use convert's -resize function to reduce the cropped tiles to the specified tile size. These resized tiles are saved to the specified output directory with the right nested directory structure expected of IIIF tiles.
+        """
         for f in tqdm(glob(temp_croppath.name + "/*"), desc="Regularizing all tiles"):
             attrs = [int(i) for i in os.path.basename(f).split(".")[0].split(",")]
 
@@ -116,6 +125,9 @@ class Tiler:
         temp_croppath.cleanup()
 
     def generate_reduced_versions(self, output_dir):
+        """
+        Create smaller derivatives of the full image.
+        """
         for ds in tqdm(self.get_downsizing_levels(), desc="Reduced versions"):
             target_directory = f"{output_dir}/full/{ds},/0"
             os.makedirs(target_directory)
@@ -158,6 +170,9 @@ class Tiler:
         json.dump(self.generate_info(), open(f"{output_dir}/info.json", "w"), indent=2)
 
     def create_iiif_0(self, output_dir):
+        """
+        Run the tiling, resizes, and info.json generation all together
+        """
         os.makedirs(output_dir)
         self.generate_cropped_tiles(output_dir)
         self.generate_reduced_versions(output_dir)
