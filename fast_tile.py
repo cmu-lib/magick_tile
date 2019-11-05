09b499bb54fce0fc0cf7cca6d210d5052f55c8ba
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-11-05 14:03:02 -0500

    rewrite into an oo program with easy args

--- fast_tile.py
+++ fast_tile.py
@@ -1,30 +1,37 @@
+"""
+This takes inspiration heavily from https://github.com/zimeon/iiif/blob/master/iiif_static.py, but uses ImageMagick rather than Pillow in order to speed up calculations at the expense of a stricter treatment of images.
+"""
+
 from glob import glob
 from tempfile import TemporaryDirectory
 import os
+import sys
 import subprocess
 from math import floor, ceil
 import json
 from tqdm import tqdm
+import optparse
 
 
 class Tiler:
+    # TODO replace this with a much smarter system for iterating potential scaling and reduction sizes
     BASE_SCALING_FACTORS = (1, 2, 4, 8, 16, 32, 64, 128, 256)
-    BASE_SMALLER_SIZES = (16, 32, 64, 128, 256, 512, 800, 1024, 2048)
+    BASE_SMALLER_SIZES = (16, 32, 64, 128, 256, 512, 1024, 2048, 4096)
 
-    def __init__(self, sourcepath, output, tile_size=256):
+    def __init__(self, sourcepath, id, tile_size=256):
         if not self.is_magick_installed():
-            raise EnvironmentError
+            raise Exception(
+                "ImageMagick does not appear to be installed or available on your $PATH"
+            )
 
         self.sourcepath = sourcepath
-        self.output = output
+        self.id = id
         self.tile_size = tile_size
-        self.temp_croppath = TemporaryDirectory()
-        self.temp_resizepath = TemporaryDirectory()
 
         self.orig_dims = [
             int(d)
             for d in subprocess.run(
-                ["identify", "-ping", f"{self.sourcepath}"], stdout=subprocess.PIPE
+                ["identify", "-ping", self.sourcepath], stdout=subprocess.PIPE
             )
             .stdout.decode("utf-8")
             .split(" ")[2]
@@ -42,12 +49,15 @@ class Tiler:
         Confirm that Imagemagick is installed and on $PATH
         """
         try:
-            subprocess.run(["convert", "--version"])
-            return true
+            res = subprocess.run(["convert", "--version"], stdout=subprocess.PIPE)
+            return True
         except:
-            return false
+            return False
 
     def get_scaling_factors(self):
+        """
+        Compute scaling factors such that the largest tile made is smaller than the shorter dimension of the input image
+        """
         return [
             sf
             for sf in self.BASE_SCALING_FACTORS
@@ -55,13 +65,14 @@ class Tiler:
         ]
 
     def get_downsizing_levels(self):
+        """
+        Compute downsizing levels such that the width of the largest reduced image is smaller than the width of the original image
+        """
         return [s for s in self.BASE_SMALLER_SIZES if s < self.orig_dims[0]]
 
-    def crop_contents(self):
-        return glob(self.temp_croppath.name + "/*")
-
-    def generate_cropped_tiles(self):
-        for sf in self.get_scaling_factors():
+    def generate_cropped_tiles(self, output_dir):
+        temp_croppath = TemporaryDirectory()
+        for sf in tqdm(self.get_scaling_factors(), desc="Creating scaling levels"):
             cropsize = self.tile_size * sf
             call = [
                 "convert",
@@ -74,13 +85,11 @@ class Tiler:
                 "%[fx:page.x],%[fx:page.y],%[fx:w],%[fx:h]",
                 "+repage",
                 "+adjoin",
-                f"{self.temp_croppath.name}/{cropsize},{sf},%[filename:tile].jpg",
+                f"{temp_croppath.name}/{cropsize},{sf},%[filename:tile].jpg",
             ]
-            crop_res = subprocess.run(call, stdout=subprocess.PIPE)
+            crop_res = subprocess.call(call, stdout=subprocess.PIPE)
 
-        for f in tqdm(
-            glob(self.temp_croppath.name + "*.jpg"), desc="Regularize cropped tiles"
-        ):
+        for f in tqdm(glob(temp_croppath.name + "/*"), desc="Regularizing all tiles"):
             attrs = [int(i) for i in os.path.basename(f).split(".")[0].split(",")]
 
             base = attrs[0]
@@ -92,162 +101,103 @@ class Tiler:
 
             file_w = ceil(w / sf) if w < self.tile_size * sf else self.tile_size
             file_h = floor(h / sf) if h < self.tile_size * sf else self.tile_size
-            target_dir = f"{self.output}/{x},{y},{w},{h}/{w},/0"
+            target_dir = f"{output_dir}/{x},{y},{w},{h}/{file_w},/0"
             os.makedirs(target_dir)
             res = subprocess.call(
                 [
                     "convert",
                     f,
-                    "-resize" f"{file_w}x{file_h}",
+                    "-resize",
+                    f"{file_w}x{file_h}",
                     f"{target_dir}/default.jpg",
-                ]
+                ],
+                stdout=subprocess.PIPE,
+            )
+        temp_croppath.cleanup()
+
+    def generate_reduced_versions(self, output_dir):
+        for ds in tqdm(self.get_downsizing_levels(), desc="Reduced versions"):
+            target_directory = f"{output_dir}/full/{ds},/0"
+            os.makedirs(target_directory)
+            res = subprocess.call(
+                [
+                    "convert",
+                    self.sourcepath,
+                    "-geometry",
+                    f"{ds}x",
+                    f"{target_directory}/default.jpg",
+                ],
+                stdout=subprocess.PIPE,
             )
 
-    def generate_reduced_versions(self):
-        for ds in self.get_downsizing_levels():
-            pass
-
     def generate_info(self):
+        """
+        Generate the info.json for this image
+        """
         return {
             "@context": "http://iiif.io/api/image/2/context.json",
-            "@id": os.path.basename(SOURCE_IMAGE).split(".")[0],
+            "@id": self.id,
             "profile": [
                 "http://iiif.io/api/image/2/level0.json",
                 {"formats": ["jpg"], "qualities": ["default"]},
             ],
             "protocol": "http://iiif.io/api/image",
-            "sizes": full_dims,
-            "tiles": [{"scaleFactors": SCALING_FACTORS, "width": self.tile_size}],
-            "width": orig_dims[0],
-            "height": orig_dims[1],
+            "sizes": [
+                {"width": ds, "height": "full"} for ds in self.get_downsizing_levels()
+            ],
+            "tiles": [
+                {"scaleFactors": self.get_scaling_factors(), "width": self.tile_size}
+            ],
+            "width": self.orig_dims[0],
+            "height": self.orig_dims[1],
         }
 
-    def write_info(self):
-        json.dump(self.generate_info(), open(f"{self.output}/info.json", "w"), indent=2)
+    def write_info(self, output_dir):
+        json.dump(self.generate_info(), open(f"{output_dir}/info.json", "w"), indent=2)
 
-    def cleanup(self):
-        """
-        Cleanup the
-        """
-        self.temp_croppath.cleanup()
-        self.temp_resizepath.cleanup()
-
-
-til = Tiler("ap.png", "output")
-til.generate_cropped_tiles()
+    def create_iiif_0(self, output_dir):
+        os.makedirs(output_dir)
+        self.generate_cropped_tiles(output_dir)
+        self.generate_reduced_versions(output_dir)
+        self.write_info(output_dir)
 
 
-# self.tile_size = 256
-# SOURCE_IMAGE = "prints_drawings.png"
-# OUTPUT_DIR = "out/prints_drawings"
+def main():
+    if sys.version_info < (3, 7):
+        sys.exit("This program requires python version 3.7 or later")
 
-# os.makedirs(OUTPUT_DIR)
-# os.makedirs(OUTPUT_DIR + "/temp_crops")
-# os.makedirs(OUTPUT_DIR + "/temp_resizes")
+    # Options and arguments
+    p = optparse.OptionParser(
+        description="IIIF Image API Level-0 static file generator",
+        usage="usage: %prog [options] file (-h for help)",
+    )
 
-# orig_dims = [
-#     int(d)
-#     for d in subprocess.run(
-#         ["identify", "-ping", f"{SOURCE_IMAGE}"], stdout=subprocess.PIPE
-#     )
-#     .stdout.decode("utf-8")
-#     .split(" ")[2]
-#     .split("x")
-# ]
+    p.add_option(
+        "--output",
+        "-o",
+        default=None,
+        action="store",
+        help="Destination directory for tiles",
+    )
 
-# if orig_dims[0] > orig_dims[1]:
-#     min_dim = orig_dims[1]
-# else:
-#     min_dim = orig_dims[0]
+    p.add_option(
+        "--identifier", "-i", default=None, action="store", help="Image identifier"
+    )
 
+    p.add_option(
+        "--tilesize",
+        "-t",
+        default=256,
+        action="store",
+        type="int",
+        help="Tile size to produce [default [%default]",
+    )
 
-# DOWNSIZING_FACTORS = []
-# p = 2
-# cont = True
-# while cont:
-#     DOWNSIZING_FACTORS.append(p)
-#     cont = (floor(min_dim / pow(2, p))) > self.tile_size
-#     p = p + 1
+    (opt, sources) = p.parse_args()
 
-
-# im_calls = []
-# for sf in SCALING_FACTORS:
-#     cropsize = self.tile_size * sf
-#     im_calls.append(
-#         f'convert {SOURCE_IMAGE} -monitor -crop {cropsize}x{cropsize} -set filename:tile "%[fx:page.x],%[fx:page.y],%[fx:w],%[fx:h]" +repage +adjoin "{OUTPUT_DIR}/temp_crops/{cropsize},{sf},%[filename:tile].jpg"'
-#     )
-
-# for call in tqdm(im_calls, desc="Creating tiles"):
-#     code = os.system(call)
-
-# image_results = []
-
-# for f in glob(OUTPUT_DIR + "/temp_crops/*.jpg"):
-#     attrs = [int(i) for i in os.path.basename(f).split(".")[0].split(",")]
-#     image_results.append(
-#         {
-#             "filename": f,
-#             "attributes": {
-#                 "base": attrs[0],
-#                 "scaling_factor": attrs[1],
-#                 "x": attrs[2],
-#                 "y": attrs[3],
-#                 "w": attrs[4],
-#                 "h": attrs[5],
-#             },
-#         }
-#     )
-
-# for i in tqdm(image_results, desc="Regularize cropped tiles"):
-#     attrs = i["attributes"]
-#     sf = attrs["scaling_factor"]
-#     real_x = attrs["x"]
-#     real_y = attrs["y"]
-#     real_w = attrs["w"]
-#     real_h = attrs["h"]
-#     file_w = ceil(attrs["w"] / sf) if attrs["w"] < self.tile_size * sf else self.tile_size
-#     file_h = floor(attrs["h"] / sf) if attrs["h"] < self.tile_size * sf else self.tile_size
-#     target_dir = f"{OUTPUT_DIR}/{real_x},{real_y},{real_w},{real_h}/{file_w},/0"
-#     os.makedirs(target_dir)
-#     res = os.system(
-#         f"convert {i['filename']} -resize {file_w}x{file_h} {target_dir}/default.jpg"
-#     )
-
-# downsize_calls = []
-# for df in DOWNSIZING_FACTORS:
-#     new_w = ceil(orig_dims[0] * 1 / pow(2, df))
-#     new_h = ceil(orig_dims[1] * 1 / pow(2, df))
-#     downsize_calls.append(
-#         f"convert {SOURCE_IMAGE} -resize {new_w}x{new_h} {OUTPUT_DIR}/temp_resizes/{new_w},{new_h}.jpg"
-#     )
-
-# for call in tqdm(downsize_calls, desc="Downsizing"):
-#     code = os.system(call)
-
-# full_results = glob(OUTPUT_DIR + "/temp_resizes/*.jpg")
-
-# full_dims = []
-
-# for i in full_results:
-#     components = os.path.basename(i).split(".")[0].split(",")
-#     target_dir = f"{OUTPUT_DIR}/full/{components[0]},/0"
-#     os.makedirs(target_dir)
-#     os.rename(i, target_dir + "/default.jpg")
-#     full_dims.append({"width": int(components[0]), "height": "full"})
+    til = Tiler(sourcepath=sources[0], id=opt.identifier, tile_size=opt.tilesize)
+    til.create_iiif_0(output_dir=opt.output)
 
 
-# manifest = {
-#     "@context": "http://iiif.io/api/image/2/context.json",
-#     "@id": os.path.basename(SOURCE_IMAGE).split(".")[0],
-#     "profile": [
-#         "http://iiif.io/api/image/2/level0.json",
-#         {"formats": ["jpg"], "qualities": ["default"]},
-#     ],
-#     "protocol": "http://iiif.io/api/image",
-#     "sizes": full_dims,
-#     "tiles": [{"scaleFactors": SCALING_FACTORS, "width": self.tile_size}],
-#     "width": orig_dims[0],
-#     "height": orig_dims[1],
-# }
-
-# json.dump(manifest, open(f"{OUTPUT_DIR}/info.json", "w"), indent=2)
+if __name__ == "__main__":
+    main()
